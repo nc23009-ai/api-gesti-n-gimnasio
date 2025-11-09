30d0bfe8786ae82daaa4351711eb208ad7c60b58
Alexander Aguilar <aguilar.rodrigo9a@gmail.com>
2025-11-08 19:28:10 -0600

    chore: update miembro, and membresia dtos

--- src/main/java/com/main/gym_api/dto/MiembroDTO.java
+++ src/main/java/com/main/gym_api/dto/MiembroDTO.java
@@ -1,21 +1,12 @@
 package com.main.gym_api.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import java.util.Date;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class MiembroDTO {
     private Long id;
-    private String nombres;
-    private String apellidos;
-    private String email;
-    private String telefono;
-    private String fechaRegistro;
-    private Boolean estadoActivo;
+    private Date fechaRegistro;
+    private boolean miembroActivo;
+    private MembresiaDTO membresia;
 }
-
